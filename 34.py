60a02f2c31b72f653fd142faf0ead87e5ede56aa
vijayalakshmiajithan3 <158257076+vijayalakshmiajithan3@users.noreply.github.com>
2024-12-18 12:14:06 +0530

    Add files via upload

--- 34.py
+++ 34.py
@@ -1,11 +1,24 @@
-square_area = lambda a: a**2
-rectangle_area = lambda l, b: l * b
-triangle_area = lambda ba, h: 0.5 * ba * h
-a = int(input("Enter the length: "))
-print("Area of the square:", square_area(a))
-l = int(input("Enter the length: "))
-b = int(input("Enter the breadth: "))
-print("Area of the triangle:", rectangle_area(l, b))
-ba = int(input("Enter the base: "))
-h = int(input("Enter the height: "))
-print("Area of the rectangle:", triangle_area(ba, h))
+class Rect:
+    def __init__(self, length, breadth):
+        self.l = length
+        self.b = breadth
+    def area (self):
+        ar=self.l * self.b
+        return ar
+    def perimeter (self):
+       p=2 * (self.l + self.b)
+       return p
+x1 = int(input("Enter length of 1st rectangle: "))
+y1 = int(input("Enter breadth of 1st rectangle: "))
+r1 = Rect(x1, y1)
+print("Area of rectangle1:", r1.area())
+print("Perimeter of rectangle1:", r1.perimeter())
+x2 = int(input("\nEnter length of 2nd rectangle:: "))
+y2 = int(input("Enter breadth of 2nd rectangle:: "))
+r2 = Rect(x2, y2)
+print("Area of rectangle2:", r2.area())
+print("Perimeter of rectangle 2:", r2.perimeter())
+if r1.area() == r2.area():
+    print ("\n Areas of both rectangles are equal.")
+else:
+    print("\n Areas of both rectangles are not equal")
